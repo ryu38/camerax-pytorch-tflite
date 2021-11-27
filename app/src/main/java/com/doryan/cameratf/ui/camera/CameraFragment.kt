7967e9ac542f6e0f4b43b1a4b93e5bfcf928785d
久賀隆矢 <niceneetlife.ryu@gmail.com>
2021-11-27 19:07:38 +0900

    implement camera preview

--- app/src/main/java/com/doryan/cameratf/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/doryan/cameratf/ui/camera/CameraFragment.kt
@@ -1,20 +1,41 @@
 package com.doryan.cameratf.ui.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.doryan.cameratf.R
 import com.doryan.cameratf.databinding.FragmentCameraBinding
+import com.google.common.util.concurrent.ListenableFuture
+import timber.log.Timber
 
 class CameraFragment: Fragment() {
 
     private val viewModel: CameraViewModel by viewModels()
-
     private lateinit var binding: FragmentCameraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // check whether all permissions granted or not
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            // if not, request required permissions here
+            requestsLauncher.launch(REQUIRED_PERMISSIONS)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,5 +50,52 @@ class CameraFragment: Fragment() {
         }
 
         return binding.root
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    private val requestsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        // Each key in permissions is like "android.permission.CAMERA"
+        if (permissions.values.all { it }) {
+            startCamera()
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder()
+            .build()
+            .also {
+                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+
+        val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+        try {
+            // Unbind use cases before rebinding
+            cameraProvider.unbindAll()
+
+            // Bind use cases to camera
+            cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview)
+
+        } catch(exc: Exception) {
+            Timber.e("Use case binding failed: ${exc}")
+        }
     }
 }
